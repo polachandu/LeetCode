4bee49da2c990e59967faf5a51cca1ad20c3b935
polachandu <86178027+polachandu@users.noreply.github.com>
2022-11-28 16:43:15 -0800

    Create Employees EarningMoreThanTheirManagers.sql

---
+++ EasySQL/Employees EarningMoreThanTheirManagers.sql
@@ -0,0 +1,30 @@
+Write an SQL query to find the employees who earn more than their managers.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+-----------+
+| id | name  | salary | managerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | Null      |
+| 4  | Max   | 90000  | Null      |
++----+-------+--------+-----------+
+Output: 
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+Explanation: Joe is the only employee who earns more than his manager.
+
+
+SELECT A.Name AS 'Employee' FROM Employee as A, Employee as B WHERE A.ManagerId = B.Id AND A.Salary > B.Salary 
