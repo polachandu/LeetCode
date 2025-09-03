385beb9fdb46e013c30382a36c4c67c23f807191
polachandu <86178027+polachandu@users.noreply.github.com>
2025-09-02 18:10:03 -0700

    Time: 8 ms (99.09%), Space: 82.6 MB (81.63%) - LeetArchive

---
+++ LeetCode/1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        boolean[] isIncomingEdgeExists = new boolean[n];
+        for (List<Integer> edge : edges) {
+            isIncomingEdgeExists[edge.get(1)] = true;
+        }
+
+        List<Integer> requireNodes = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            if (!isIncomingEdgeExists[i]) {
+                requireNodes.add(i);
+            }
+        }
+        return requireNodes;
+    }
+}
