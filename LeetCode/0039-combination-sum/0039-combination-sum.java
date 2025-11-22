23e342167a0d135c1e81f6b6de68919381978bfe
polachandu <86178027+polachandu@users.noreply.github.com>
2025-11-22 11:02:04 -0800

    Time: 3 ms (25.54%), Space: 46.6 MB (6.1%) - LeetArchive

---
+++ LeetCode/0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+       List<List<Integer>> results = new ArrayList();
+       LinkedList<Integer> comb = new LinkedList();
+        Arrays.sort(candidates);
+       backTrack(target,comb, 0, candidates, results);
+
+       return results;
+    }
+
+    private void backTrack(int remain, LinkedList<Integer> comb, int start, int[] candidates, List<List<Integer>> results){
+        if(remain == 0){
+            results.add(new ArrayList<>(comb));
+        }
+        if(remain < 0){
+            return;
+        }
+
+        for(int i=start;i<candidates.length;i++){
+            if(candidates[i] > remain) break;
+            comb.add(candidates[i]);
+            backTrack(remain-candidates[i],comb,i,candidates,results);
+            comb.removeLast();
+        }
+    }
+}
