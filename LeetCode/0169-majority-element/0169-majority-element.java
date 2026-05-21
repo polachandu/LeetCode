d3ce3c638bb5eb2f57d2488fa96fb2cd0b01dfbe
polachandu <86178027+polachandu@users.noreply.github.com>
2026-05-21 14:16:36 -0700

    Time: 12 ms (34.46%), Space: 52.5 MB (90.41%) - LeetArchive

---
+++ LeetCode/0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                count = entry.getKey();
+            }
+        }
+        return count;
+    }
+}
