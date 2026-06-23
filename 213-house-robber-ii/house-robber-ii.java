f981155e998d51b753e590107086713e56728657
polachandu <86178027+polachandu@users.noreply.github.com>
2026-06-23 15:30:46 -0700

    Time: 0 ms (100.00%) | Memory: 42.9 MB (21.59%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,19 +2,21 @@ class Solution {
     public int rob(int[] nums) {
         if (nums.length == 1)
             return nums[0];
-        int excludeFirst = robLinear(nums, 1, nums.length - 1);
-        int excludeLast = robLinear(nums, 0, nums.length - 2);
-        return Math.max(excludeFirst, excludeLast);
+        int excludeLast = robHouse(nums, 0, nums.length - 2);
+        int excludeFirst = robHouse(nums, 1, nums.length - 1);
+        return Math.max(excludeLast, excludeFirst);
     }
 
-    private int robLinear(int[] nums, int first, int last) {
-        int[] dp = new int[last - first + 1];
+    private int robHouse(int[] nums, int first, int last) {
+        int length = last - first + 1;
+        int[] dp = new int[length];
         dp[0] = nums[first];
-        if(dp.length == 1) return dp[0];
-        dp[1] = Math.max(nums[first], nums[first + 1]);
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[first + i] + dp[i - 2]);
+        if (length > 1) {
+            dp[1] = Math.max(nums[first], nums[first + 1]);
         }
-        return dp[dp.length - 1];
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[first + i]);
+        }
+        return dp[length - 1];
     }
 }
