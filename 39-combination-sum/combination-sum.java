e75a801b6845f6b96f926dca39bafaafcf4d73a0
polachandu <86178027+polachandu@users.noreply.github.com>
2026-06-23 14:35:50 -0700

    Time: 5 ms (8.31%) | Memory: 45.8 MB (33.21%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -7,17 +7,20 @@ class Solution {
         return results;
     }
 
-    private void backTrack(List<List<Integer>> results, List<Integer> current, int[] nums, int target, int start) {
+    private void backTrack(List<List<Integer>> results, List<Integer> current, int[] candidates, int target,
+            int start) {
         if (target == 0) {
             results.add(new ArrayList(current));
             return;
         }
+
         if (target < 0) {
             return;
         }
-        for (int i = start; i < nums.length; i++) {
-            current.add(nums[i]);
-            backTrack(results, current, nums, target - nums[i], i);
+
+        for (int i = start; i < candidates.length; i++) {
+            current.add(candidates[i]);
+            backTrack(results, current, candidates, target - candidates[i], i);
             current.remove(current.size() - 1);
         }
     }
