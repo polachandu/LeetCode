18e22d36ab4835d54370740568caa71af09cd245
polachandu <86178027+polachandu@users.noreply.github.com>
2026-06-25 14:26:54 -0700

    Time: 4 ms (45.50%) | Memory: 52.2 MB (58.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,9 @@
 class Solution {
-
     public int numIslands(char[][] grid) {
         int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+                if (dfs(grid, i, j)) {
                     count++;
                 }
             }
@@ -13,14 +11,17 @@ class Solution {
         return count;
     }
 
-    private void dfs(char[][] grid, int row, int col) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length
-                || grid[row][col] != '1')
-            return;
+    private boolean dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return false;
+        }
+
         grid[row][col] = '0';
         dfs(grid, row + 1, col);
         dfs(grid, row - 1, col);
         dfs(grid, row, col + 1);
         dfs(grid, row, col - 1);
+
+        return true;
     }
 }
