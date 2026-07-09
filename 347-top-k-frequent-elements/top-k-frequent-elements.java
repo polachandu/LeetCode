a7f4f95ffd94164228cf54f7ff821c664c598f05
polachandu <86178027+polachandu@users.noreply.github.com>
2026-07-09 14:50:00 -0700

    Time: 16 ms (39.63%) | Memory: 47.8 MB (32.02%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -4,8 +4,7 @@ class Solution {
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        PriorityQueue<Integer> minHeap = new PriorityQueue((a, b) -> map.get(a) - map.get(b));
+        PriorityQueue<Integer> minHeap = new PriorityQueue((a, b) -> (map.get(a) - map.get(b)));
 
         for (int key : map.keySet()) {
             minHeap.offer(key);
