0282b541a0ded5a430013fd6151e322ae2e782fe
polachandu <86178027+polachandu@users.noreply.github.com>
2026-07-14 14:36:40 -0700

    Time: 9 ms (83.04%) | Memory: 47.9 MB (61.60%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -8,42 +8,42 @@
  * }
  */
 public class Codec {
-
     StringBuilder sb = new StringBuilder();
+    int index = 0;
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        dfs(root, sb);
+        construct(root, sb);
         return sb.toString();
     }
 
-    private void dfs(TreeNode node, StringBuilder sb) {
-        if (node == null) {
+    private void construct(TreeNode root, StringBuilder sb) {
+        if (root == null) {
             sb.append("null,");
             return;
         }
-        sb.append(node.val + ",");
-        dfs(node.left, sb);
-        dfs(node.right, sb);
+        sb.append(root.val + ",");
+        construct(root.left, sb);
+        construct(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         String[] nodes = data.split(",");
-        int[] index = { 0 };
-        return build(nodes, index);
+        return build(nodes);
     }
 
-    private TreeNode build(String[] nodes, int[] index) {
-        if (nodes[index[0]].equals("null")) {
-            index[0]++;
+    private TreeNode build(String[] nodes) {
+        if (nodes[index].equals("null")) {
+            index++;
             return null;
         }
-        TreeNode node = new TreeNode(Integer.parseInt(nodes[index[0]++]));
-        node.left = build(nodes, index);
-        node.right = build(nodes, index);
+        TreeNode node = new TreeNode(Integer.parseInt(nodes[index++]));
+        node.left = build(nodes);
+        node.right = build(nodes);
         return node;
     }
+
 }
 
 // Your Codec object will be instantiated and called as such:
