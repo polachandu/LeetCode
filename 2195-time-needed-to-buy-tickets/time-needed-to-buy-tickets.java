a562b6de813903812fe56d3b7c2d265b39567949
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-01 11:48:10 -0700

    Time: 8 ms (42.62%) | Memory: 43 MB (63.77%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Deque<int[]> deque = new ArrayDeque();
+        for (int i = 0; i < tickets.length; i++) {
+            deque.offerLast(new int[] { i, tickets[i] });
+        }
+        int time = 0;
+        while (!deque.isEmpty()) {
+            int[] front = deque.pollFirst();
+            front[1]--;
+            time++;
+            if (front[0] == k && front[1] == 0) {
+                return time;
+            }
+            if (front[1] > 0) {
+                deque.offerLast(front);
+            }
+        }
+        return time;
+    }
+}
