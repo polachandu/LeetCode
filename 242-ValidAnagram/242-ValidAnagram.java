5801a7aa04be2294e0871c4b16389af888c21c88
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:15:49 -0700

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,19 @@
+// Last updated: 8/12/2026, 11:15:49 AM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<Character, Integer> sMap = new HashMap();
+        for (int i = 0; i < s.length(); i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char tChar = t.charAt(i);
+            if (!sMap.containsKey(tChar) || sMap.get(tChar) == 0) {
+                return false;
+            }
+            sMap.put(tChar, sMap.getOrDefault(tChar, 0) - 1);
+        }
+        return true;
+    }
+}
