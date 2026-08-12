fc6993bd2c2f8b92bbf0c1f54127e03c58a274c2
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:12:39 -0700

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.java
@@ -0,0 +1,23 @@
+// Last updated: 8/12/2026, 11:12:39 AM
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = Arrays.stream(piles).max().getAsInt();
+        while (left < right) {
+            int mid = left + ((right - left) / 2);
+            if (canFinish(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean canFinish(int[] piles, int mid, int h) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += (int) Math.ceil((double) pile / mid);
+        }
+        return hours <= h;
+    }
+}
