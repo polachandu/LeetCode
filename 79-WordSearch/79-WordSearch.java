8de8792b3cf3d53cf6fbb07a2a15d35bd8808b60
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:18:41 -0700

    Create 79-WordSearch

---
+++ 79-WordSearch/79-WordSearch.java
@@ -0,0 +1,38 @@
+// Last updated: 8/12/2026, 11:18:41 AM
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int row, int col, int start) {
+        if (start == word.length()) {
+            return true;
+        }
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
+            return false;
+        }
+
+        if (board[row][col] != word.charAt(start)) {
+            return false;
+        }
+        char temp = board[row][col];
+        board[row][col] = '\0';
+
+        boolean innRes = dfs(board, word, row + 1, col, start + 1) ||
+                dfs(board, word, row - 1, col, start + 1) ||
+                dfs(board, word, row, col + 1, start + 1) ||
+                dfs(board, word, row, col - 1, start + 1);
+
+        board[row][col] = temp;
+        return innRes;
+    }
+}
