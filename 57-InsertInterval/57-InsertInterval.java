1ca6cf379f3828b2b14f047c171e9289c3219336
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:19:14 -0700

    Create 57-InsertInterval

---
+++ 57-InsertInterval/57-InsertInterval.java
@@ -0,0 +1,24 @@
+// Last updated: 8/12/2026, 11:19:14 AM
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList();
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+
+        result.add(newInterval);
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
