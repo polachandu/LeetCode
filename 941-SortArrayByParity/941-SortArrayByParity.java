b52a9831c028f48767ed80ef78806c6b8361a661
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:12:30 -0700

    Create 941-SortArrayByParity

---
+++ 941-SortArrayByParity/941-SortArrayByParity.java
@@ -0,0 +1,24 @@
+// Last updated: 8/12/2026, 11:12:29 AM
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        ArrayList list1 = new ArrayList<>();
+		ArrayList list2 = new ArrayList<>();
+	        for(int i=0;i<nums.length;i++) {
+	        	if(nums[i]%2 ==0) {
+	        		list1.add(nums[i]);
+	        	}
+	        }
+	        for(int i=0;i<nums.length;i++) {
+	        	if(nums[i]%2 !=0) {
+	        		list2.add(nums[i]);
+	        	}
+	        }
+	        list1.addAll(list2);
+	        int[] newNums = new int[nums.length];
+	        for(int i=0;i<list1.size();i++) {
+	        	newNums[i] = (int) list1.get(i);
+	        }
+	        
+	       return newNums; 
+    }
+}
