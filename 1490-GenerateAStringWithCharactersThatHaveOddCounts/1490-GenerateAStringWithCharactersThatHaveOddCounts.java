511c1c825795cc4870e36ddb2c7a65dccdb2dd9f
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:10:54 -0700

    Create 1490-GenerateAStringWithCharactersThatHaveOddCounts

---
+++ 1490-GenerateAStringWithCharactersThatHaveOddCounts/1490-GenerateAStringWithCharactersThatHaveOddCounts.java
@@ -0,0 +1,21 @@
+// Last updated: 8/12/2026, 11:10:54 AM
+class Solution {
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+	        if(n%2 ==0 && n != 1) {
+	        	for(int i=0;i<n-1;i++) {
+	        		sb.append("a");
+	        	}
+	        	sb.append("b");
+	        }else if(n%2 != 0 && n != 1) {
+	        	for(int i=0;i<n-2;i++) {
+	        		sb.append("a");
+	        	}
+	        	sb.append("bc");
+	        }
+        if(n ==1 ){
+            sb.append("a");
+        }
+	        return sb.toString();
+    }
+}
