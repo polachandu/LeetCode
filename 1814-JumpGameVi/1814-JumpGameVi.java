a6f8bc6391428edc3400063b8f7578ea722c3967
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:09:30 -0700

    Create 1814-JumpGameVi

---
+++ 1814-JumpGameVi/1814-JumpGameVi.java
@@ -0,0 +1,25 @@
+// Last updated: 8/12/2026, 11:09:30 AM
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        Deque<Integer> deque = new ArrayDeque();
+        deque.offerLast(0);
+
+        for (int i = 1; i < n; i++) {
+            if (deque.peekFirst() < i - k) {
+                deque.pollFirst();
+            }
+
+            dp[i] = nums[i] + dp[deque.peekFirst()];
+
+            while (!deque.isEmpty() && dp[deque.peekLast()] <= dp[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        return dp[n - 1];
+    }
+}
