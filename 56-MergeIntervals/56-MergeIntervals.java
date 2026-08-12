df17942a105e86313afe575f783ab387b645f5f5
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:19:16 -0700

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -0,0 +1,20 @@
+// Last updated: 8/12/2026, 11:19:16 AM
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 1)
+            return intervals;
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        List<int[]> result = new ArrayList();
+        result.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] prev = result.get(result.size() - 1);
+            if (prev[1] >= intervals[i][0]) {
+                prev[1] = Math.max(prev[1], intervals[i][1]);
+            } else {
+                result.add(intervals[i]);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
