fbe48924ab1baa81bf6538a8ee82cc36be1bce13
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:08:08 -0700

    Create 2454-LargestLocalValuesInAMatrix

---
+++ 2454-LargestLocalValuesInAMatrix/2454-LargestLocalValuesInAMatrix.java
@@ -0,0 +1,23 @@
+// Last updated: 8/12/2026, 11:08:07 AM
+class Solution {
+    public int[][] largestLocal(int[][] grid) {
+        int N = grid.length;
+        int[][] maxLocal = new int[N-2][N-2];
+        for(int i=0; i < N-2;i++){
+            for(int j=0; j<N-2;j++){
+                maxLocal[i][j] = findMax(grid,i,j);
+            }
+        }
+        return maxLocal;
+    }
+
+    private int findMax(int[][] grid,int x,int y){
+        int maxElement = 0;
+        for(int i=x;i<x+3;i++){
+            for(int j=y;j<y+3;j++){
+                maxElement = Math.max(maxElement, grid[i][j]);
+            }
+        }
+        return maxElement;
+    }
+}
