00dd15d7ed873626e960ae463b3cf3b0f48320ab
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:08:49 -0700

    Create 2020-RemoveOneElementToMakeTheArrayStrictlyIncreasing

---
+++ 2020-RemoveOneElementToMakeTheArrayStrictlyIncreasing/2020-RemoveOneElementToMakeTheArrayStrictlyIncreasing.java
@@ -0,0 +1,45 @@
+// Last updated: 8/12/2026, 11:08:49 AM
+class Solution {
+    public boolean canBeIncreasing(int[] nums) {
+  int num[]=new int[nums.length-1];
+		boolean f=false;
+		if(asc(nums)) {
+			return true;
+		}
+	for(int i=0;i<nums.length-1;i++) {
+		if(nums[i]>=nums[i+1]) {
+			if(i>0) {
+			if(nums[i+1]<=nums[i-1]) {
+				System.arraycopy(nums, 0, num, 0, i+1);
+				System.arraycopy(nums, i+2, num, i+1,nums.length-i-2);
+				System.out.println(Arrays.toString(num));
+				f=asc(num);
+				break;
+			}}
+			System.arraycopy(nums, 0, num, 0, i);
+			System.arraycopy(nums, i+1, num, i,nums.length-i-1);
+			System.out.println(Arrays.toString(num));
+			f=asc(num);
+				}
+	}
+		return f;
+}
+
+private static boolean asc(int[] num) {
+	boolean flag=false;
+	if(num.length==1) {
+		return true;
+	}
+	for(int j=0;j<num.length-1;j++) {
+		if(num[j]<num[j+1]) {
+			flag=true;}
+		else {
+			flag=false;
+			break;
+		}
+	}
+	return flag;
+             }
+    }
+
+    
