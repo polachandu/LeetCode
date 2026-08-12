59022b7aa30cab2fe29ba0b7fce51eefb694e928
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:18:48 -0700

    Create 77-Combinations

---
+++ 77-Combinations/77-Combinations.java
@@ -0,0 +1,26 @@
+// Last updated: 8/12/2026, 11:18:48 AM
+class Solution {
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> comb = new ArrayList<>();
+        backtrack(comb, new ArrayList<>(), n, k, 1);
+
+        return comb;
+    }
+
+    public void backtrack(List<List<Integer>> comb, List<Integer> list, int range, int combinations, int index) {
+
+        if (list.size() == combinations) {
+            comb.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = index; i <= range; i++) {
+
+            list.add(i);
+            backtrack(comb, list, range, combinations, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
