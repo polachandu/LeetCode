bf2188bc20649783b37ef913458779d67ad7bc96
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:07:39 -0700

    Create 3193-MaximumStrongPairXorI

---
+++ 3193-MaximumStrongPairXorI/3193-MaximumStrongPairXorI.java
@@ -0,0 +1,14 @@
+// Last updated: 8/12/2026, 11:07:39 AM
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+        int ans = 0;
+        for(int x: nums){
+            for(int y: nums){
+                if(Math.abs(x-y) <= Math.min(x,y)){
+                    ans = Math.max(ans, x ^ y);
+                }
+            }
+        }
+        return ans;
+    }
+}
