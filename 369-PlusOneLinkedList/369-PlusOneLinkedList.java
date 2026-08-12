b90489bfa3ded708c3098ee9f0ee1bd210a56d3d
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:15:06 -0700

    Create 369-PlusOneLinkedList

---
+++ 369-PlusOneLinkedList/369-PlusOneLinkedList.java
@@ -0,0 +1,35 @@
+// Last updated: 8/12/2026, 11:15:06 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode plusOne(ListNode head) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+        ListNode notNine = sentinel;
+
+        while(head != null){
+            if(head.val != 9){
+                notNine = head;
+            }
+            head = head.next;
+        }
+
+        notNine.val++;
+        notNine = notNine.next;
+
+        while(notNine != null){
+            notNine.val = 0;
+            notNine = notNine.next;
+        }
+
+        return sentinel.val != 0 ? sentinel : sentinel.next;
+    }
+}
