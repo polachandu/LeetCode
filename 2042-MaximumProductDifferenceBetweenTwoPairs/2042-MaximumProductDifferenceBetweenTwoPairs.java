72a706a07836ec371bb230ea6ffab02e7635c452
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:08:47 -0700

    Create 2042-MaximumProductDifferenceBetweenTwoPairs

---
+++ 2042-MaximumProductDifferenceBetweenTwoPairs/2042-MaximumProductDifferenceBetweenTwoPairs.java
@@ -0,0 +1,7 @@
+// Last updated: 8/12/2026, 11:08:46 AM
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-1]*nums[nums.length-2] - nums[0]*nums[1];
+    }
+}
