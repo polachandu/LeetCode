eac5216634caebac2c18cf39e86b672d7431a162
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:16:26 -0700

    Create 207-CourseSchedule

---
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -0,0 +1,34 @@
+// Last updated: 8/12/2026, 11:16:26 AM
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList());
+        }
+        for (int[] pre : prerequisites) {
+            indegree[pre[0]]++;
+            graph.get(pre[1]).add(pre[0]);
+        }
+
+        Queue<Integer> queue = new LinkedList();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            count++;
+            for (int nei : graph.get(curr)) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    queue.add(nei);
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
