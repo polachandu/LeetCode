e1a95b3dc402861c93a66ef1a9863fc5cd0494f1
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:15:09 -0700

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.java
@@ -0,0 +1,16 @@
+// Last updated: 8/12/2026, 11:15:09 AM
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i] == nums2[j]){
+                    al.add(nums1[i]);
+                }
+            }
+        }
+        Set<Integer> res = new HashSet<>(al);
+        int[] result = res.stream().mapToInt(Integer::intValue).toArray();
+        return result;
+    }
+}
