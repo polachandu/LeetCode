78032f74feaaa147967cdb99c0b24e16f62c6bb8
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:18:04 -0700

    Create 108-ConvertSortedArrayToBinarySearchTree

---
+++ 108-ConvertSortedArrayToBinarySearchTree/108-ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,39 @@
+// Last updated: 8/12/2026, 11:18:04 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int[] nums;
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        this.nums = nums;
+        return helper(0,nums.length-1);    
+    }
+
+    public TreeNode helper(int left, int right){
+
+        if(left>right) return null;
+
+        int p = (left+right)/2;
+        if((left+right)%2 == 1) ++p;
+
+        TreeNode root = new TreeNode(nums[p]);
+        root.left = helper(left,p-1);
+        root.right = helper(p+1,right);
+        return root;
+
+    }
+}
