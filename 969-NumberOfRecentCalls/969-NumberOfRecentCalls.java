705700faaf3874483ac96333030427d2048628fc
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:12:24 -0700

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.java
@@ -0,0 +1,23 @@
+// Last updated: 8/12/2026, 11:12:24 AM
+class RecentCounter {
+    LinkedList<Integer> slideWindow;
+
+    public RecentCounter() {
+        this.slideWindow = new LinkedList<Integer>(); 
+    }
+    
+    public int ping(int t) {
+        this.slideWindow.addLast(t);
+
+        while(this.slideWindow.getFirst() < t-3000){
+            this.slideWindow.removeFirst();
+        }
+        return this.slideWindow.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
