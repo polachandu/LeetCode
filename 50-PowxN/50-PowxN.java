8dfeef065e281efc6ecb6da8ab978a127dce20e7
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:19:26 -0700

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,11 @@
+// Last updated: 8/12/2026, 11:19:26 AM
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n < 0 && n > Integer.MIN_VALUE) {
+            x = 1/x;
+            n *= -1;
+        }
+        return n%2==0 ? myPow(x*x,n/2) :x*myPow(x*x,n/2);
+    }
+}
