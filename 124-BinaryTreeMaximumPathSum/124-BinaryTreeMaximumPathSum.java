3289aa39a283fade9d21e02f9c3b35727053ba61
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:17:49 -0700

    Create 124-BinaryTreeMaximumPathSum

---
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.java
@@ -0,0 +1,39 @@
+// Last updated: 8/12/2026, 11:17:49 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Integer maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null)
+            return 0;
+        gain(root);
+        return maxSum;
+    }
+
+    private int gain(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int leftGain = Math.max(gain(root.left), 0);
+        int rightGain = Math.max(gain(root.right), 0);
+
+        int pathSum = root.val + leftGain + rightGain;
+        maxSum = Math.max(pathSum, maxSum);
+
+        return root.val + Math.max(leftGain, rightGain);
+    }
+}
