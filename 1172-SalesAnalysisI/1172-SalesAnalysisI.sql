0c08093337aa34063945fb4289127b6bbd557865
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:11:46 -0700

    Create 1172-SalesAnalysisI

---
+++ 1172-SalesAnalysisI/1172-SalesAnalysisI.sql
@@ -0,0 +1,23 @@
+-- Last updated: 8/12/2026, 11:11:46 AM
+# Write your MySQL query statement below
+WITH aggregated_sales AS (
+  SELECT 
+    seller_id, 
+    SUM(price) AS total_price 
+  FROM 
+    Sales 
+  GROUP BY 
+    seller_id
+)
+
+SELECT 
+  seller_id 
+FROM 
+  aggregated_sales 
+WHERE 
+  total_price = (
+    SELECT 
+      MAX(total_price) 
+    FROM 
+      aggregated_sales
+  );
