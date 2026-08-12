3f7735bc731d83c39dea918402957f930ef3a467
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:10:42 -0700

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ 1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,17 @@
+// Last updated: 8/12/2026, 11:10:41 AM
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> al = new ArrayList<>();
+        int max=0,sum =0, totalCandies=0;
+        for(int i=0;i<candies.length;i++){
+                totalCandies = candies[i]+extraCandies;
+                max = Arrays.stream(candies).max().getAsInt();
+                if(totalCandies >= max){
+                    al.add(true);
+                }else{
+                    al.add(false);
+                }
+        }
+        return al;
+    }
+}
