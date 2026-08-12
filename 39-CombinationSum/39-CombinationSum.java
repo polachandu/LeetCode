037e1f0ea338b55258d92054e349880f60e6750f
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:19:49 -0700

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.java
@@ -0,0 +1,28 @@
+// Last updated: 8/12/2026, 11:19:48 AM
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> results = new ArrayList();
+        List<Integer> current = new ArrayList();
+        backTrack(results, current, candidates, target, 0);
+        return results;
+    }
+
+    private void backTrack(List<List<Integer>> results, List<Integer> current, int[] candidates, int target,
+            int start) {
+        if (target == 0) {
+            results.add(new ArrayList(current));
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            current.add(candidates[i]);
+            backTrack(results, current, candidates, target - candidates[i], i);
+            current.remove(current.size() - 1);
+        }
+    }
+}
