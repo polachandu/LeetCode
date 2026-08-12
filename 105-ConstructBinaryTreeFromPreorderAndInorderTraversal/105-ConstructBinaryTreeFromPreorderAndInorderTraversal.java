8a8f27098dfa9f40aa937dd9426d50a461273972
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-12 11:18:10 -0700

    Create 105-ConstructBinaryTreeFromPreorderAndInorderTraversal

---
+++ 105-ConstructBinaryTreeFromPreorderAndInorderTraversal/105-ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,41 @@
+// Last updated: 8/12/2026, 11:18:09 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int idx = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        TreeNode result = build(preorder, map, 0, inorder.length - 1);
+        return result;
+    }
+
+    private TreeNode build(int[] preorder, Map<Integer, Integer> map, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int rootVal = preorder[idx++];
+        TreeNode root = new TreeNode(rootVal);
+        int mid = map.get(rootVal);
+
+        root.left = build(preorder, map, left, mid - 1);
+        root.right = build(preorder, map, mid + 1, right);
+        return root;
+    }
+}
