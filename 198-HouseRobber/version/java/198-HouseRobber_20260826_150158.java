625585360bf6d3ff87fe5ed14c0f2187ac2a42e7
polachandu <86178027+polachandu@users.noreply.github.com>
2026-08-26 15:01:58 -0700

    Create 198-HouseRobber

---
+++ 198-HouseRobber/version/java/198-HouseRobber_20260826_150158.java
@@ -0,0 +1,17 @@
+// Last updated: 8/26/2026, 3:01:58 PM
+1class Solution {
+2    public int rob(int[] nums) {
+3        if (nums.length == 1) {
+4            return nums[0];
+5        }
+6        int[] dp = new int[nums.length];
+7        dp[0] = nums[0];
+8        if (nums.length >= 1) {
+9            dp[1] = Math.max(nums[0], nums[1]);
+10        }
+11        for (int i = 2; i < nums.length; i++) {
+12            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+13        }
+14        return dp[nums.length - 1];
+15    }
+16}
