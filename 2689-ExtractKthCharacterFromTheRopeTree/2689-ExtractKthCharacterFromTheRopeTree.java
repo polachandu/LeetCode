b30a6baeea66c1b1d7979d4c56c0f1a04726608e
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:28:09 -0700

    Create 2689-ExtractKthCharacterFromTheRopeTree

---
+++ 2689-ExtractKthCharacterFromTheRopeTree/2689-ExtractKthCharacterFromTheRopeTree.java
@@ -0,0 +1,43 @@
+// Last updated: 9/5/2026, 12:28:08 PM
+/**
+ * Definition for a rope tree node.
+ * class RopeTreeNode {
+ *     int len;
+ *     String val;
+ *     RopeTreeNode left;
+ *     RopeTreeNode right;
+ *     RopeTreeNode() {}
+ *     RopeTreeNode(String val) {
+ *         this.len = 0;
+ *         this.val = val;
+ *     }
+ *     RopeTreeNode(int len) {
+ *         this.len = len;
+ *         this.val = "";
+ *     }
+ *     RopeTreeNode(int len, RopeTreeNode left, RopeTreeNode right) {
+ *         this.len = len;
+ *         this.val = "";
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public char getKthCharacter(RopeTreeNode root, int k) {
+        StringBuilder sb = new StringBuilder();
+        dfs(root, sb);
+        return sb.toString().charAt(k-1);
+    }
+
+    private void dfs(RopeTreeNode root, StringBuilder sb) {
+
+        if (root != null) {
+            if (root.left == null && root.right == null) {
+                sb.append(root.val);
+            }
+            dfs(root.left, sb);
+            dfs(root.right, sb);
+        }
+    }
+}
