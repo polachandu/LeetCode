7a5db1c9079adba9d630bdcc60145395677baea7
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:32:56 -0700

    Create 797-AllPathsFromSourceToTarget

---
+++ 797-AllPathsFromSourceToTarget/797-AllPathsFromSourceToTarget.java
@@ -0,0 +1,28 @@
+// Last updated: 9/5/2026, 12:32:56 PM
+class Solution {
+    private int target;
+    private int[][] graph;
+    private List<List<Integer>> results;
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        this.target = graph.length-1;
+        this.graph = graph;
+        this.results = new ArrayList<List<Integer>>();
+        LinkedList<Integer> path = new LinkedList<Integer>();
+        path.add(0);
+        backtrack(0,path);
+        return this.results;
+    }
+
+    private void backtrack(int curNode, LinkedList<Integer> path){
+        if(curNode == this.target){
+            this.results.add(new ArrayList<Integer>(path));
+            return;
+        }
+
+        for(int nextNode: this.graph[curNode]){
+            path.addLast(nextNode);
+            backtrack(nextNode,path);
+            path.removeLast();
+        }
+    }
+}
