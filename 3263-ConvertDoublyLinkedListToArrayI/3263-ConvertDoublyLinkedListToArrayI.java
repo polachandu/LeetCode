536ef460854cfff2ec014896de56a70ede813656
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:27:49 -0700

    Create 3263-ConvertDoublyLinkedListToArrayI

---
+++ 3263-ConvertDoublyLinkedListToArrayI/3263-ConvertDoublyLinkedListToArrayI.java
@@ -0,0 +1,28 @@
+// Last updated: 9/5/2026, 12:27:49 PM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+};
+*/
+
+class Solution {
+    public int[] toArray(Node head) {
+        int count = 0;
+
+        Node curr = head;
+        while(curr != null){
+            count++;
+            curr = curr.next;
+        }
+        int[] res = new int[count];
+        int temp = 0;
+        while(head != null){
+            res[temp++] = head.val;
+            head = head.next;
+        }
+    return res;
+    }
+}
