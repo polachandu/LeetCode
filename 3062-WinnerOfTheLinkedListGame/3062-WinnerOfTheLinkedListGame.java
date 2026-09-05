2253679af7bbfad2ff5bc2afb86dc25904d98383
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:27:50 -0700

    Create 3062-WinnerOfTheLinkedListGame

---
+++ 3062-WinnerOfTheLinkedListGame/3062-WinnerOfTheLinkedListGame.java
@@ -0,0 +1,37 @@
+// Last updated: 9/5/2026, 12:27:50 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public String gameResult(ListNode head) {
+        ListNode even = head;
+        int countEven = 0;
+        int countOdd = 0;
+        while(even != null){
+            ListNode odd = even.next;
+            if(even.val > odd.val){
+                countEven++;
+            }else if(even.val < odd.val){
+                countOdd++;
+            }
+            else{
+                continue;
+            }
+            even = odd.next;
+        }
+        if(countEven > countOdd){
+            return "Even";
+        }else if(countOdd > countEven){
+            return "Odd";
+        }else{
+            return "Tie";
+        }
+    }
+}
