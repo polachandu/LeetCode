7c6976a72f4e54f19d7fd7d0e15c8439f041ee80
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:32:06 -0700

    Create 981-TimeBasedKeyValueStore

---
+++ 981-TimeBasedKeyValueStore/981-TimeBasedKeyValueStore.java
@@ -0,0 +1,40 @@
+// Last updated: 9/5/2026, 12:32:06 PM
+class TimeMap {
+
+    Map<String, List<String[]>> store;
+
+    public TimeMap() {
+        store = new HashMap();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        store.putIfAbsent(key, new ArrayList());
+        store.get(key).add(new String[] { String.valueOf(timestamp), value });
+    }
+
+    public String get(String key, int timestamp) {
+        if (!store.containsKey(key))
+            return "";
+        List<String[]> list = store.get(key);
+        String result = "";
+        int left = 0, right = list.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midTime = Integer.parseInt(list.get(mid)[0]);
+            if (midTime <= timestamp) {
+                result = list.get(mid)[1];
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
