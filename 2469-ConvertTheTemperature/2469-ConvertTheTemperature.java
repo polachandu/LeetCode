896302b018735b37381039a0902d246c515f5447
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:28:21 -0700

    Create 2469-ConvertTheTemperature

---
+++ 2469-ConvertTheTemperature/2469-ConvertTheTemperature.java
@@ -0,0 +1,11 @@
+// Last updated: 9/5/2026, 12:28:21 PM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] res = new double[2];
+        double kelvin = celsius + 273.15;
+        double fahrenheit = (celsius*1.80)+32.00;
+        res[0] = kelvin;
+        res[1] = fahrenheit;
+        return res;
+    }
+}
