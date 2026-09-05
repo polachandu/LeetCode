4c7ea84d21dc94482b26d7928797f5865454a819
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:32:08 -0700

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 9/5/2026, 12:32:08 PM
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1 || n==2){
+            return 1;
+        }
+        else if(n > 2){
+            return fib(n-1)+fib(n-2);
+        }else{
+            return 0;
+        }
+        
+    }
+}
