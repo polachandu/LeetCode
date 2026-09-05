92dbae6ce1a3daaa21ef6d250a5314f1cdcaf75d
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:31:45 -0700

    Create 1079-LetterTilePossibilities

---
+++ 1079-LetterTilePossibilities/1079-LetterTilePossibilities.java
@@ -0,0 +1,25 @@
+// Last updated: 9/5/2026, 12:31:45 PM
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] charCount = new int[26];
+        for(char c: tiles.toCharArray()){
+            charCount[c-'A']++;
+        } 
+        return findSequences(charCount);
+    }
+
+    private int findSequences(int[] charCount){
+        int totalCount = 0;
+
+        for(int i=0;i<26;i++){
+            if(charCount[i] == 0){
+                continue;
+            }
+            totalCount++;
+            charCount[i]--;
+            totalCount += findSequences(charCount);
+            charCount[i]++;
+        }
+        return totalCount;
+    }
+}
