db915e977ca112e6165692c7140cb28d87b59614
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:29:02 -0700

    Create 1913-MaximumProductDifferenceBetweenTwoPairs

---
+++ 1913-MaximumProductDifferenceBetweenTwoPairs/1913-MaximumProductDifferenceBetweenTwoPairs.java
@@ -0,0 +1,7 @@
+// Last updated: 9/5/2026, 12:29:02 PM
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-1]*nums[nums.length-2] - nums[0]*nums[1];
+    }
+}
