c6418a02224ea7cf889334e9cd47aac5f702d92a
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:29:14 -0700

    Create 1828-QueriesOnNumberOfPointsInsideACircle

---
+++ 1828-QueriesOnNumberOfPointsInsideACircle/1828-QueriesOnNumberOfPointsInsideACircle.java
@@ -0,0 +1,22 @@
+// Last updated: 9/5/2026, 12:29:14 PM
+class Solution {
+    public int[] countPoints(int[][] points, int[][] queries) {
+        int[] a = new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int count =0;
+            int x = queries[i][0];
+            int y = queries[i][1];
+            int radius = queries[i][2];
+            for(int j=0;j<points.length;j++){
+                int x1 = points[j][0];
+                int y1 = points[j][1];
+                if(((x-x1)*(x-x1) +(y-y1)*(y-y1))<=(radius*radius)){
+                    count++;
+                }
+            }
+            a[i] = count;
+        }
+        return a;
+    }
+    
+}
