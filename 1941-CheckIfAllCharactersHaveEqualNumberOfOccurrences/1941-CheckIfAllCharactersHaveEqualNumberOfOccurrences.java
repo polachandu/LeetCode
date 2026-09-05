ca03c13cf6ae546698e97c32de900507a542852f
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:28:59 -0700

    Create 1941-CheckIfAllCharactersHaveEqualNumberOfOccurrences

---
+++ 1941-CheckIfAllCharactersHaveEqualNumberOfOccurrences/1941-CheckIfAllCharactersHaveEqualNumberOfOccurrences.java
@@ -0,0 +1,22 @@
+// Last updated: 9/5/2026, 12:28:59 PM
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        
+        int[] alph = new int[26];
+        int occurance = 0;
+        
+        for(int i =0;i< s.length();i++){
+            alph[s.charAt(i)-'a']++;
+            occurance = Math.max(occurance,alph[s.charAt(i)-'a']);
+        }
+        
+        for(int i = 0;i<26;i++){
+            if(alph[i] != 0 && alph[i] != occurance) {
+                return false;
+        }
+        }
+        return true;
+    }
+    
+
+}
