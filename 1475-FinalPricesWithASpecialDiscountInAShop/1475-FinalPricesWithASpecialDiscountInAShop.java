5325e5b0b7dc9eb2d9beb2c8c2ec7764549f898e
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:30:42 -0700

    Create 1475-FinalPricesWithASpecialDiscountInAShop

---
+++ 1475-FinalPricesWithASpecialDiscountInAShop/1475-FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,15 @@
+// Last updated: 9/5/2026, 12:30:42 PM
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        for(int i=0;i<prices.length;i++) {
+	        	for(int j=i+1;j<prices.length;j++) {
+	        		if(prices[i]>=prices[j]) {
+	        			prices[i] -=prices[j];
+	        			break;
+	        		}
+	        		
+	        	}
+	        }
+	        return prices;
+    }
+}
