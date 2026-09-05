9e20d752ed679cbbaab3ec285ecb545905798bec
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:28:11 -0700

    Create 2703-ReturnLengthOfArgumentsPassed

---
+++ 2703-ReturnLengthOfArgumentsPassed/2703-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 9/5/2026, 12:28:10 PM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
