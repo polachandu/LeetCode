2e83aa19fe1c8c40e84e916d5c290f31c3a517d6
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:30:19 -0700

    Create 1557-MinimumNumberOfVerticesToReachAllNodes

---
+++ 1557-MinimumNumberOfVerticesToReachAllNodes/1557-MinimumNumberOfVerticesToReachAllNodes.java
@@ -0,0 +1,17 @@
+// Last updated: 9/5/2026, 12:30:19 PM
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        boolean[] isIncomingEdgeExists = new boolean[n];
+        for (List<Integer> edge : edges) {
+            isIncomingEdgeExists[edge.get(1)] = true;
+        }
+
+        List<Integer> requireNodes = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            if (!isIncomingEdgeExists[i]) {
+                requireNodes.add(i);
+            }
+        }
+        return requireNodes;
+    }
+}
