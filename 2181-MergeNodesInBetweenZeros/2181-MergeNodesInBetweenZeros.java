e59d2d647507e754e513dc382e444670ae3bb730
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:28:36 -0700

    Create 2181-MergeNodesInBetweenZeros

---
+++ 2181-MergeNodesInBetweenZeros/2181-MergeNodesInBetweenZeros.java
@@ -0,0 +1,33 @@
+// Last updated: 9/5/2026, 12:28:36 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        
+        ListNode newNode = new ListNode(0);
+        ListNode newtemp = newNode;
+        ListNode temp = head.next;
+        int sum = 0;
+        if(head == null && head.next == null) return head;
+        while(temp != null){
+            sum += temp.val;
+            if(temp.val == 0){
+                ListNode node = new ListNode(sum);
+                newtemp.next = node;
+                newtemp = newtemp.next;
+                sum = 0;
+            }
+
+           temp = temp.next;
+        }
+        return newNode.next;
+    }
+}
