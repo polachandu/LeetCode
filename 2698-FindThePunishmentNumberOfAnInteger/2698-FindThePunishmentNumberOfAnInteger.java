7f8d7d9b3edac4deece840e9f8b9a3189ecbee7c
polachandu <86178027+polachandu@users.noreply.github.com>
2026-09-05 12:28:13 -0700

    Create 2698-FindThePunishmentNumberOfAnInteger

---
+++ 2698-FindThePunishmentNumberOfAnInteger/2698-FindThePunishmentNumberOfAnInteger.java
@@ -0,0 +1,26 @@
+// Last updated: 9/5/2026, 12:28:12 PM
+class Solution {
+    public int punishmentNumber(int n) {
+        int punishNum = 0;
+        for(int currentNum = 1;currentNum<=n;currentNum++){
+            int squareNum = currentNum*currentNum;
+            if(canPartition(squareNum, currentNum)){
+                punishNum+=squareNum;
+            } 
+        }
+        return punishNum;
+    }
+
+    private boolean canPartition(int num, int target){
+        if(target < 0 || num < target){
+            return false;
+        }
+        if(num == target){
+            return true;
+        }
+
+        return(canPartition(num/10,target - (num%10)) ||
+        canPartition(num/100,target-(num%100)) ||
+        canPartition(num/1000,target-(num%1000)));
+    }
+}
